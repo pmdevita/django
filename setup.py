1fad12a5e9d08f9abff31feb352484ceec3e88bc
Peter DeVita <mewtwo2643@yahoo.com>
2025-03-20 16:45:55 -0400

    Cython: Only build on CPython

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
+import platform
+
 from Cython.Build import cythonize
 from setuptools import setup
 
+ext_modules = []
+
+if platform.python_implementation() == "CPython":
+    ext_modules.append(cythonize(["django/db/models/query.py"]))
+
 setup(
     name="Django",
-    ext_modules=cythonize(["django/db/models/query.py"]),
+    ext_modules=ext_modules,
 )
